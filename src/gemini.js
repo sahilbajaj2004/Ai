9a5411d889970a992268551322c47f9e9b3b5e77
Sahil bajaj <sahilbajaj0941@gmail.com>
2025-03-27 14:06:29 +0530

    Initial commit

---
+++ src/gemini.js
@@ -0,0 +1,36 @@
+let apiKey = "AIzaSyDIALwlrXUVUZIgQmXoKmx1Bt02_XVWTIo"
+
+import {
+  GoogleGenerativeAI,
+  // HarmCategory,
+  // HarmBlockThreshold,
+} from "@google/generative-ai";
+
+
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash-8b",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 20,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
+  });
+  // const hindiPrompt = `Please respond in Hindi: ${prompt}
+
+  const result = await chatSession.sendMessage(prompt);
+  return result.response.text()
+}
+
+export default run;
